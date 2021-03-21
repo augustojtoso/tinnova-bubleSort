9f7ada98b3574bfdfe8a687c6b36d4b32ddf0ab0
Augusto Toso <augustojtoso@gmail.com>
2021-03-20 21:54:22 -0300

    initial commit

---
+++ src/test/java/com/tinnova/bubleSort/BubleSortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinnova.bubleSort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubleSortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
